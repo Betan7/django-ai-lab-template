0b71a95af0809b59378a465a815ec585128598d7
Betan7 <113317845+Betan7@users.noreply.github.com>
2025-08-29 22:14:34 +0000

    estudiantes

--- estudiantes/admin.py
+++ estudiantes/admin.py
@@ -1,21 +1,33 @@
 from django.contrib import admin
+from .models import Programa, Curso, Estudiante, Inscripcion
 
-from .models import Programa, Curso, Estudiante
 
 @admin.register(Programa)
 class ProgramaAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'descripcion')
+    list_display = ('id', 'nombre', 'descripcion')
     search_fields = ('nombre',)
+    ordering = ('nombre',)
+
 
 @admin.register(Curso)
 class CursoAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'programa')
+    list_display = ('id', 'nombre', 'programa', 'descripcion')
     list_filter = ('programa',)
     search_fields = ('nombre',)
+    ordering = ('programa', 'nombre')
+
 
 @admin.register(Estudiante)
 class EstudianteAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'email', 'programa')
+    list_display = ('id', 'nombre', 'email', 'programa')
     list_filter = ('programa',)
     search_fields = ('nombre', 'email')
-    filter_horizontal = ('cursos',)  
+    ordering = ('nombre',)
+
+
+@admin.register(Inscripcion)
+class InscripcionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'estudiante', 'curso', 'fecha')
+    list_filter = ('curso', 'fecha')
+    search_fields = ('estudiante__nombre', 'curso__nombre')
+    ordering = ('-fecha',)
